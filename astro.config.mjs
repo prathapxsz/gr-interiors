402247afc120443ae268d2ceb136f4930e26f278
prathapxsz <prathapxsz@gmail.com>
2026-02-17 22:01:01 +0530

    added flow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://prathapxsz.github.io',
+  base: '/gr-interiors',
+
   vite: {
     plugins: [tailwindcss()]
   },
